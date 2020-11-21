b92405cd7d9784beb4efa8cf7e956f6f9c912013
Superklez <55788247+Superklez@users.noreply.github.com>
2020-11-21 15:07:11 +0800

    Update SuperNet.py
    Added necessary terms for L2 regularization which affects the cost function and dW and db in back propagation.

--- SuperNet.py
+++ SuperNet.py
@@ -32,10 +32,20 @@ class NeuralNetwork():
         dadz[z > 0] = 1
         return dadz
 
-    def get_cost(self, Y):
+    def get_cost(self, Y, C=0):
         m = Y.shape[1]
         logprobs = Y * np.log(self.A['A' + str(self.L)]) + (1 - Y) * np.log(1 - self.A['A' + str(self.L)])
-        return -1/m * np.sum(logprobs)
+        log_term = -1/m * np.sum(logprobs)
+        # IF WITH REGULARIZATION
+        if C != 0:
+            Wl_norm_vals = []
+            for l in range(1, self.L+1):
+                Wl_norm = np.sum(np.square(self.W['W' + str(l)]))
+                Wl_norm_vals.append(Wl_norm)
+            reg_term = C/(2*m) * np.sum(Wl_norm_vals)
+            return log_term + reg_term
+        # IF NO REGULARIZATION
+        return log_term
 
     def forward_propagation(self, X, activation='relu'):
         self.Z = {}
@@ -47,10 +57,7 @@ class NeuralNetwork():
         self.Z['Z' + str(self.L)] = np.dot(self.W['W' + str(self.L)], self.A['A' + str(self.L-1)]) + self.b['b' + str(self.L)]
         self.A['A' + str(self.L)] = self.sigmoid(self.Z['Z' + str(self.L)])
 
-    def forward_propagation_with_regularization(self, X, activation='relu', C=0, keep_prob=1):
-        pass
-
-    def backward_propagation(self, Y, activation='relu'):
+    def backward_propagation(self, Y, C=0, activation='relu'):
         m = Y.shape[1]
         self.dZ = {}
         self.dA = {}
@@ -58,16 +65,28 @@ class NeuralNetwork():
         self.db = {}
         self.dA['dA' + str(self.L)] = -Y/self.A['A' + str(self.L)] + (1-Y)/(1-self.A['A' + str(self.L)])
         self.dZ['dZ' + str(self.L)] = self.dA['dA' + str(self.L)] * self.sigmoid_derivative(self.Z['Z' + str(self.L)])
-        self.dW['dW' + str(self.L)] = 1/m * np.dot(self.dZ['dZ' + str(self.L)], self.A['A' + str(self.L-1)].T)
+        # IF NO REGULARIZATION
+        if C == 0:
+            self.dW['dW' + str(self.L)] = 1/m * np.dot(self.dZ['dZ' + str(self.L)], self.A['A' + str(self.L-1)].T)
+        # IF WITH REGULARIZATION
+        if C != 0:
+            backprop_term = 1/m * np.dot(self.dZ['dZ' + str(self.L)], self.A['A' + str(self.L-1)].T)
+            reg_term = C/m * self.W['W' + str(self.L)]
+            self.dW['dW' + str(self.L)] = backprop_term + reg_term
         self.db['db' + str(self.L)] = 1/m * np.sum(self.dZ['dZ' + str(self.L)], axis=1, keepdims=True)
+
         for l in reversed(range(1, self.L)):
             self.dA['dA' + str(l)] = np.dot(self.W['W' + str(l + 1)].T, self.dZ['dZ' + str(l + 1)])
             self.dZ['dZ' + str(l)] = self.dA['dA' + str(l)] * self.relu_derivative(self.Z['Z' + str(l)])
-            self.dW['dW' + str(l)] = 1/m * np.dot(self.dZ['dZ' + str(l)], self.A['A' + str(l-1)].T)
+            # IF NO REGULARIZATION
+            if C == 0:
+                self.dW['dW' + str(l)] = 1/m * np.dot(self.dZ['dZ' + str(l)], self.A['A' + str(l-1)].T)
+            # IF WITH REGULARIZATION
+            if C != 0:
+                backprop_term = 1/m * np.dot(self.dZ['dZ' + str(l)], self.A['A' + str(l-1)].T)
+                reg_term = C/m * self.W['W' + str(l)]
+                self.dW['dW' + str(l)] = backprop_term + reg_term
             self.db['db' + str(l)] = 1/m * np.sum(self.dZ['dZ' + str(l)], axis=1, keepdims=True)
-
-    def backward_propagation_with_regularization(self, Y, activation='relu', C=0, keep_prob=1):
-        pass
 
     def update_parameters(self, alpha):
         for l in range(1, self.L+1):
@@ -98,13 +117,13 @@ class NeuralNetwork():
             if C != 0 or keep_prob != 1:
                 self.forward_propagation_with_regularization()
                 self.backward_propagation_with_regularization()
-                cost = self.get_cost(y_train)
+                cost = self.get_cost(y_train, C)
                 self.costs.append(cost)
             else:
                 self.forward_propagation(X_train)
                 self.backward_propagation(y_train)
                 self.update_parameters(alpha)
-                cost = self.get_cost(y_train)
+                cost = self.get_cost(y_train, C)
                 self.costs.append(cost)
 
             # PRINT COST FOR FEEDBACK WHILE TRAINING
